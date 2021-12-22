4321a69b8142934f05782f2d6e75bbb5907b45a8
thomps0189 <angelathompson2011@gmail.com>
2021-12-22 16:09:50 -0600

    link to index.html with other route

--- server.js
+++ server.js
@@ -38,6 +38,10 @@ app.get("/notes", function (req, res) {
     res.sendFile(path.join(__dirname, "/public/notes.html"))
 });
 
+app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "./public/index.html"));
+});
+
 app.listen(3001, () => {
     console.log(`API server now on port 3001!`);
 });
