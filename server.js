caf02b3ea45d785974d8635b7f2bca2ff9ef4d04
thomps0189 <angelathompson2011@gmail.com>
2021-12-26 18:14:49 -0600

    update

--- server.js
+++ server.js
@@ -1,11 +1,10 @@
 const express = require("express");
-const PORT = process.env.PORT || 3001;
-const app = express();
 const morgan = require("morgan");
+const app = express();
 const {v4: uuidv4} = require("uuid");
 const path = require("path");
+const PORT = process.env.PORT || 3001;
 const fs = require('fs');
-// const { notes } = require("./db/db.json");
 
 app.use(morgan("tiny"));
 
